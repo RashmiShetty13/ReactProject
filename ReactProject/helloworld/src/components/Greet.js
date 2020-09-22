c607b7ecefec988311cf009a37d0700698335e8f
rashmishetty <rashmishetty.r22@gmail.com>
2020-09-23 01:00:18 +0530

    React Project
    React Project

---
+++ ReactProject/helloworld/src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1> Hello Rashmi Shetty</h1>
+// }
+const Greet = () => <h1> Hello Rashmi Shetty</h1>
+// const Greet =props => {
+//     console.log(props)
+//     return <h1> Hello {props.name}</h1>
+// }
+
+export default Greet
